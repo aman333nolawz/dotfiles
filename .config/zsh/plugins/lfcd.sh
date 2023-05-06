dd97ac8ba3bb9786e8529423ce7181d4b9812478
Nolawz <67265008+aman333nolawz@users.noreply.github.com>
2023-05-06 17:31:34 +0530

    Added lfcd in zsh

---
+++ .config/zsh/plugins/lfcd.sh
@@ -0,0 +1,18 @@
+# Change working dir in shell to last dir in lf on exit.
+
+lfcd () {
+    tmp="$(mktemp)"
+    # `command` is needed in case `lfcd` is aliased to `lf`
+    command lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir"
+            fi
+        fi
+    fi
+}
+
+bindkey -s '^o' 'lfcd\n'
