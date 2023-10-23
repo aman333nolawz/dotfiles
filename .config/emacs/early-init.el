735c6d380fc5e31507028e2cd731ea634a313489
Nolawz <67265008+aman333nolawz@users.noreply.github.com>
2023-10-23 11:07:04 +0530

    Switched to swaybg and vscode

--- .config/emacs/early-init.el
+++
@@ -1,6 +0,0 @@
-(setq package-enable-at-startup nil)
-
-(when (fboundp 'startup-redirect-eln-cache)
-  (startup-redirect-eln-cache
-   (convert-standard-filename
-    (expand-file-name  "var/eln-cache/" user-emacs-directory))))
