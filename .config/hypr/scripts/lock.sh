e7bda10eab1c614546055142518519f9437ba7d4
Nolawz <67265008+aman333nolawz@users.noreply.github.com>
2023-10-25 19:53:04 +0530

    Added effects to screenlock

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -9,27 +9,33 @@ lavender='#b4befe'
 cyan='#89dceb'
 green='#a6e3a1'
 
+image=$(find ~/.config/wallpapers/ -type f | shuf -n1)
+
 swaylock \
-  --color=$background \
-  --inside-color=$selection$alpha \
-  --inside-ver-color=$green$alpha \
-  --inside-wrong-color=$red$alpha \
-  --inside-clear-color=$orange$alpha \
-  --inside-caps-lock-color=$yellow$alpha \
-  --ring-color=$lavender$alpha \
-  --ring-ver-color=$green$alpha \
-  --ring-wrong-color=$red$alpha \
-  --ring-clear-color=$orange$alpha \
-  --ring-caps-lock-color=$yellow$alpha \
-  --key-hl-color=$yellow$alpha \
-  --bs-hl-color=$orange$alpha \
-  --caps-lock-bs-hl-color=$orange$alpha \
-  --caps-lock-key-hl-color=$orange$alpha \
-  --separator-color=$selection$alpha \
-  --text-color=$selection \
+  --image "$image" \
+  --clock --timestr "%H:%M" --datestr "%d/%m/%y" \
+  --effect-blur 7x5 \
+	--effect-vignette 0.3:0.9 \
+  --inside-color=$selection \
+  --inside-ver-color=$green \
+  --inside-wrong-color=$red \
+  --inside-clear-color=$orange \
+  --inside-caps-lock-color=$yellow \
+  --ring-color=$lavender \
+  --ring-ver-color=$green \
+  --ring-wrong-color=$red \
+  --ring-clear-color=$orange \
+  --ring-caps-lock-color=$yellow \
+  --key-hl-color=$yellow \
+  --bs-hl-color=$orange \
+  --caps-lock-bs-hl-color=$orange \
+  --caps-lock-key-hl-color=$orange \
+  --separator-color=$selection \
+  --text-color=$lavender \
   --text-caps-lock-color=$selection \
   --indicator-idle-visible \
   --indicator-caps-lock \
-  --indicator-radius=120 \
+  --indicator-radius=130 \
   --indicator-thickness=5 \
-  --layout-bg-color=$background$alpha
+  --layout-bg-color=$background \
+  --hide-keyboard-layout
